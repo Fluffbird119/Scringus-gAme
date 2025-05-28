b15c20544876ac24e669e43dbac9fc2daa29902b
Fluffbird119 <jaysanthony@gmail.com>
2025-05-28 01:42:53 -0500

    added comments

--- Assets/MapGenScript.cs
+++ Assets/MapGenScript.cs
@@ -25,21 +25,17 @@ public class MapGenScript : MonoBehaviour
         drawRoom(colorList);
     }
 
-    /**
-     * 
-     */
+    //  Generates a list with the size map width by map height of random colors to make each room
     private Color32[] initializeColorArray()
     {
         Color32[] colorList = new Color32[MAP_WIDTH * MAP_HEIGHT];
         for (int i = 0; i < MAP_WIDTH * MAP_HEIGHT; i++)
         {
-
             byte r = (byte)Random.Range(0, 255);
             byte g = (byte)Random.Range(0, 255);
             byte b = (byte)Random.Range(0, 255);
 
             Color32 newColor = new Color32(r, g, b, 255);
-
 
             colorList[i] = newColor;
         }
@@ -50,6 +46,7 @@ public class MapGenScript : MonoBehaviour
     
     private void initializeRoomMap()
     {
+        // Makes "roomMap" array from 1 to the max size
         int roomNumber = 1;
         for (int x = 0; x < MAP_WIDTH; x++)
         {
@@ -59,6 +56,8 @@ public class MapGenScript : MonoBehaviour
                 roomNumber++;
             }
         }
+
+        // goes right to left top to bottom to see if rooms can merge and make connected large rooms
         for (int x = 0; x < MAP_WIDTH; x++)
         {
             for (int y = 0; y < MAP_HEIGHT; y++)
@@ -79,7 +78,7 @@ public class MapGenScript : MonoBehaviour
         }
     }
 
-
+    // actually puts all the rooms on screen using the color list for the colors and the numbers from the roomMap to determine what colors they are
     private void drawRoom(Color32[] colorList)
     {
         for (int x = 0; x < MAP_WIDTH; x++)
@@ -96,11 +95,5 @@ public class MapGenScript : MonoBehaviour
                 Room room = new Room(roomPrefab, roomPos);
             }
         }
-    }
-
-
-    void Update()
-    {
-        
     }
 }
