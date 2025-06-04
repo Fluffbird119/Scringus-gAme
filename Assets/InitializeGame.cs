5e5a0e90c8b1ef602d5c63b13b88071ee6850410
SeekerOfCode00 <143357920+SeekerOfCode00@users.noreply.github.com>
2025-06-03 23:23:42 -0500

    Created basic framework Enemies and Game Initialization

---
+++ Assets/InitializeGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // MapGenScript.generateMaps
+        // SpawnEnemies.firstWave(MapGenScript.getRooms())
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
