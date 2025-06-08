c0d652a9fb42cb2b3a73559481c33becb655c86b
Luke Menard <lmenard4@huskers.unl.edu>
2025-06-08 03:43:13 -0500

    Merging player

--- Assets/CameraFollowPlayer.cs
+++ Assets/CameraFollowPlayer.cs
@@ -7,16 +7,17 @@ public class CameraFollowPlayer : MonoBehaviour
     public Transform player;
     public float smoothTime = 0.3F;
     Vector3 velocity = Vector3.zero;
+    private float visionSize = 5f;
 
     void Start()
     {
-        transform.position = player.position + new Vector3(0, 0, -1.5f);
+        transform.position = player.position + new Vector3(0, 0, -visionSize);
     }
 
     void Update()
     {
         
-        Vector3 targetPosition = player.TransformPoint(new Vector3(0, 0, -1.5f));
+        Vector3 targetPosition = player.TransformPoint(new Vector3(0, 0, -visionSize));
 
         // Smoothly move the camera towards that target position
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
