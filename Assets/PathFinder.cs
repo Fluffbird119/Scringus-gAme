123f4604c76951c849776fa6e246a6ba0117b4c0
Fluffbird119 <jaysanthony@gmail.com>
2025-06-08 17:28:16 -0500

    Revert "Reapply "Merge branch 'Jay's-Branch' into Player""
    This reverts commit fa2c372f07c5e8ac8655d8e9301fa37e308aa1a0.

--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
@@ -297,23 +296,21 @@ public class PathFinder : MonoBehaviour
         var startPair = new Tuple<int, Room>(0, start);
         queue.Enqueue(startPair);
         parents[start] = null;
-        int test = 0;
         while (queue.Count > 0)
         {
             var pair = queue.Dequeue();
             var room = pair.Item2;
-            visited[room.row(), room.col()] = true;
+            visited[(int)(room.getPos().y / Room.ROOM_UNIT), (int)(room.getPos().x / Room.ROOM_UNIT)] = true;
 
             if (room.Equals(end)) //for some reason, start and end are equal
             {
-                return ShufflePath(GetPath(parents, pair));
+                return GetPath(parents, pair);
             }
             else
             {
                 var neighbors = GetUnvisitedNeighbors(room, grid, visited);
                 foreach (var neighbor in neighbors)
                 {
-                    test++;
                     var neighborRoom = neighbor.Item2;
                     if (!parents.ContainsKey(neighborRoom)) 
                     {
@@ -323,24 +320,8 @@ public class PathFinder : MonoBehaviour
                 }
             }
         }
-        Debug.Log(test);
+
         return null;
-    }
-
-
-
-    public static List<int> ShufflePath(List<int> path)
-    {
-        int n = path.Count;
-        while (n > 1)
-        {
-            n--;
-            int k = Random.Range(0, n + 1);
-            int value = path[k];
-            path[k] = path[n];
-            path[n] = value;
-        }
-        return path;
     }
 
     private static List<int> GetPath(Dictionary<Room, Tuple<int, Room>> parents, Tuple<int, Room> destinationPair)
@@ -397,33 +378,6 @@ public class PathFinder : MonoBehaviour
     public static List<Wall> findWallsAlongPath(List<int> path, Room start, Room end, Room[,] rooms)
     {
         List<Wall> walls = new List<Wall>();
-        Room currRoom = start;
-        if (path != null)
-        {
-            foreach (int direction in path)
-            {
-                if (currRoom.getWalls().ContainsKey(direction) && !areRoomsConnected(currRoom, rooms[0, MapGenScript.MAP_WIDTH / 2]))
-                {
-                    walls.Add(currRoom.getWalls()[direction]);
-                }
-                switch (direction)
-                {
-                    case 1:
-                        currRoom = rooms[currRoom.row() - 1, currRoom.col()];
-                        break;
-                    case 2:
-                        currRoom = rooms[currRoom.row(), currRoom.col() + 1];
-                        break;
-                    case 3:
-                        currRoom = rooms[currRoom.row() + 1, currRoom.col()];
-                        break;
-                    case 4:
-                        currRoom = rooms[currRoom.row(), currRoom.col() - 1];
-                        break;
-
-                }
-            }
-        }
         return walls;
     }
 
