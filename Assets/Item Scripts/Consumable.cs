496aab08e89d5ee0c8b676d118a7d92d3560cca2
Justin-Bobbit <jaacon06@gmail.com>
2025-06-09 12:13:14 -0500

    made some classes for the weapon types and organized them into a folder
    The classes still need to be coded

---
+++ Assets/Item Scripts/Consumable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//made this too early, no point in deleting it since we'll need it eventually, but don't touch it until MVP is done
+public class Consumable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
